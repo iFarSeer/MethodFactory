a538fd9597497dcf69c492a89790a1c3bb9dc64f
farseer <farseer.spade@gmail.com>
2016-04-17 19:06:57 +0800

    [update]:移除无效空行

--- src/com/farseer/pattern/store/HamburgStore.java
+++ src/com/farseer/pattern/store/HamburgStore.java
@@ -8,17 +8,15 @@ import com.farseer.pattern.hamburg.Hamburg;
  */
 public abstract class HamburgStore {
 
-
-
     public Hamburg orderHamburg(String type) {
 
         Hamburg hamburg = createHamburg(type);
         hamburg.prepare();
         hamburg.make();
         hamburg.box();
-
         return hamburg;
     }
 
+    //工厂方法
     protected abstract Hamburg createHamburg(String type);
 }
